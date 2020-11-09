3e4a8636ba386cc94c5e9afe1b8a320b713bda47
raidentek <33987212+raidentek@users.noreply.github.com>
2020-11-08 19:54:52 -0500

    upload

--- cliente/hooks/useFiltro.js
+++ cliente/hooks/useFiltro.js
@@ -1,21 +1,31 @@
-import React, { useEffect }  from "react";
+import React, { useEffect, useState } from "react";
 import clienteAxios from "../config/clienteAxios";
-
+import { Form } from "react-bootstrap";
 
 function useFiltro() {
+  const [dataCategorias, setDataCategorias] = useState([]);
 
-    const obteneCategorias = async () => {
-        const rex = await clienteAxios.get('categorias')
-        console.log(rex.data);
-    }
+  const obteneCategorias = async () => {
+    const rex = await clienteAxios.get("categorias");
+    setDataCategorias(rex.data);
+    console.log(rex.data);
+  };
 
-    useEffect(() => {
-    obteneCategorias()
-    }, [])
+  useEffect(() => {
+    obteneCategorias();
+  }, []);
 
   const FiltroUI = () => (
     <div>
       <h4>filtro</h4>
+      <Form.Group controlId="exampleForm.ControlSelect1">
+        <Form.Label>Selecciona una Categoria</Form.Label>
+        <Form.Control as="select">
+          {dataCategorias.map((e_cat) => (
+            <option key={e_cat._id}> {e_cat.nombre} </option>
+          ))}
+        </Form.Control>
+      </Form.Group>
     </div>
   );
 
