d669e449d38c4cbee70069ef18cb439e93398907
raidentek <33987212+raidentek@users.noreply.github.com>
2020-11-12 16:37:38 -0500

    modal

--- cliente/components/indexHome/Modal.js
+++ cliente/components/indexHome/Modal.js
@@ -1,19 +1,42 @@
 import React, { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
-function Modal() {
-  const [modal, setModal] = useState(true);
-
-  const ModalChange = () => {
-    setModal(modal ? false : true);
-  };
-
+function Modal({ modal, closeModal, ModalChange }) {
   return (
+    // <div >
+    //     <button className='btn' onClick={()=> ModalChange()}  >modal</button>
+    //   <div
+    //     className={`${
+    //       !modal
+    //       ? "modal fixed top-0 left-0 w-full h-screen grid place-items-center invisible"
+    //       : "modal fixed top-0 left-0 w-full h-screen grid place-items-center visible"
+    //     }`}
+    //     style={{ background: "rgba(0, 0, 0, 0.5)" }}
+    //   >
+    //     <div className="relative bg-gray-100 w-11/12 md:w-2/3 mx-auto rounded grid place-items-center h-64">
+    //       <h1>Modal Content</h1>
+    //       <button className="absolute top-0 right-0 m-4" onClick={closeModal}>
+    //         <FaTimes className="text-red-400 text-4xl" />
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
+
     <div>
-      <button className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white m-5 show-modal">
+      <button
+        className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white m-5 "
+        onClick={() => ModalChange()}
+      >
         show modal
       </button>
 
-      <div className="modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 hidden">
+      <div
+        className={`${
+          !modal
+            ? "modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 hidden"
+            : "modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 visible"
+        }`}
+      >
         {/* <!-- modal --> */}
         <div className="bg-white rounded shadow-lg w-1/3">
           {/* <!-- modal header --> */}
@@ -29,7 +52,7 @@ function Modal() {
             exercitationem voluptatum praesentium laboriosam nam.
           </div>
           <div className="flex justify-end items-center w-100 border-t p-3">
-            <button className="bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-white mr-1 close-modal">
+            <button className="bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-white mr-1" onClick={closeModal} >
               Cancel
             </button>
             <button className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white">
@@ -43,23 +66,3 @@ function Modal() {
 }
 
 export default Modal;
-
-{
-  /* 
-
-  const modal = document.querySelector('.modal');
-
-  const showModal = document.querySelector('.show-modal');
-  const closeModal = document.querySelectorAll('.close-modal');
-
-  showModal.addEventListener('click', function (){
-    modal.classList.remove('hidden')
-  });
-
-  closeModal.forEach(close => {
-    close.addEventListener('click', function (){
-      modal.classList.add('hidden')
-    });
-  });
- */
-}
--- cliente/components/layout/Mainx.js
+++ cliente/components/layout/Mainx.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import Avatar from "../indexHome/Avatar";
 import Lista from "../indexHome/Lista";
 import Modal from "../indexHome/Modal";
 
 
 function Mainx() {
+
+  const [modal, setModal] = useState(false);
+
+    const ModalChange = () => {
+      setModal(!modal);
+    };
+
+  const modalOpen = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
   return (
-    <div className="h-full w-full  bg-black px8 overflow-hidden  ">
+    <div className="h-full w-full  bg-black px8 overflow-hidden "     >
       <div className="">
-        <Modal/>
-      <Avatar/>
+        <Modal ModalChange={ModalChange} closeModal={closeModal} modal={modal}  />
+      <Avatar  />
       <Lista/>
       <Lista/>
       </div>
