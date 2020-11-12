b6d23b6aebabde5289a842baf42b71bc66ed285c
raidentek <33987212+raidentek@users.noreply.github.com>
2020-11-12 07:27:52 -0500

    librerias

--- cliente/components/layout/Sidebar.js
+++ cliente/components/layout/Sidebar.js
@@ -13,6 +13,7 @@ export const navList = [
   { icon: <FaPenSquare />, href: "/casas", menu: "Formulario" },
   { icon: <FaSearch />, href: "/", menu: "Busqueda" },
   { icon: <FaTable />, href: "/tabla", menu: "Tabla" },
+  { icon: <FaOutdent />, href: "/tabla", menu: "Tabla" },
 ];
 
 export const linksDerecho = [
@@ -22,16 +23,36 @@ export const linksDerecho = [
 
 function Sidebar() {
   return (
-    <div className="h-full w-64 bg-black flex-shrink-0">
-      <h1 className="text-gray-400 text-3xl text-center mt-4">Spotify</h1>
-      <div>
-        <ul>
+    <div
+      className="h-full w-56 flex-shrink-0"
+      style={{ backgroundColor: "#242528" }}
+    >
+      <h1 className="text-gray-400 text-3xl text-center mt-4">Tidal</h1>
+      <div className="">
+        <ul className="w-full px-3 my-4 ">
+          <h1 className="text-gray-400 pl-2 my-1 font-bold ">Mis Music </h1>
           {navList.map((nav, i) => (
-            <li className='nav-linkx text-gray-400 ' > 
+            <li className="text-gray-400 w-48 ">
               <Link href={nav.href} key={i}>
-                  <a className='nav-linkx no-underline ' >
-                      {nav.icon}
-                       {nav.menu} </a>
+                <a className="nav-link w-full rounded-xl hover:text-white hover:bg-gray-700 ">
+                  <span className="mx-2">{nav.icon}</span>
+                  {nav.menu}{" "}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <hr className="border border-gray-100" />
+
+        <ul className="w-full px-3 my-4">
+          <h1 className="text-gray-400 pl-2 my-1 font-bold ">Mis Listas </h1>
+          {linksDerecho.map((nav, i) => (
+            <li className=" text-gray-400 w-full  ">
+              <Link href={nav.href} key={i}>
+                <a className="nav-link w-full rounded-xl hover:text-white hover:bg-gray-700 ">
+                  <span className="mx-2">{nav.icon}</span>
+                  {nav.menu}{" "}
+                </a>
               </Link>
             </li>
           ))}
