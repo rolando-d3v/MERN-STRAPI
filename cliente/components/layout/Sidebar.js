aa2511a107d01d48c5196b7503670485deb135a5
raidentek <33987212+raidentek@users.noreply.github.com>
2020-11-12 21:52:06 -0500

    tidal

--- cliente/components/layout/Sidebar.js
+++ cliente/components/layout/Sidebar.js
@@ -27,12 +27,17 @@ function Sidebar() {
       className="h-full w-56 flex-shrink-0"
       style={{ backgroundColor: "#242528" }}
     >
-      <h1 className="text-gray-400 text-3xl text-center mt-4">Tidal</h1>
+      <div className="flex items-center my-4 mx-2 ">
+        <img src="/tidal.png" alt="red" className="w-12 h-12 mx-2 " />
+        <Link href="/">
+          <a className="text-gray-400 text-3xl font-extrabold text-center tracking-widest ">Tidal</a>
+        </Link>
+      </div>
       <div className="">
-        <ul className="w-full px-3 my-4 ">
-          <h1 className="text-gray-400 pl-2 my-1 font-bold ">Mis Music </h1>
+        <ul className="w-full px-3 my-6 ">
+          <h1 className="text-white pl-2 my-1 font-bold ">Mis Music </h1>
           {navList.map((nav, i) => (
-            <li key={i}  className="text-gray-400 w-48 ">
+            <li key={i} className="text-gray-400 w-48 ">
               <Link href={nav.href} key={i}>
                 <a className="nav-link w-full rounded-xl hover:text-white hover:bg-gray-700 ">
                   <span className="mx-2">{nav.icon}</span>
@@ -45,7 +50,7 @@ function Sidebar() {
         <hr className="border border-gray-100" />
 
         <ul className="w-full px-3 my-4">
-          <h1 className="text-gray-400 pl-2 my-1 font-bold ">Mis Listas </h1>
+          <h1 className="text-white pl-2 my-1 font-bold ">Mis Listas </h1>
           {linksDerecho.map((nav, i) => (
             <li key={i} className=" text-gray-400 w-full  ">
               <Link href={nav.href} key={i}>
